0f389fe22baa799e3e37272846d9a1d94fc7535c
Jyonasan59 <joloend.villapando@gmail.com>
2024-07-19 22:24:29 +0800

    remove download report, filter and import 2

--- DEVELOPMENT/incomming-project/user-solar-details.php
+++ DEVELOPMENT/incomming-project/user-solar-details.php
@@ -69,19 +69,6 @@
                           <li class="breadcrumb-item active" aria-current="page">Product-Details</li>
                       </ol>
                   </div>
-                  <div class="d-flex">
-                    <div class="justify-content-center">
-                        <button type="button" class="btn btn-white btn-icon-text my-2 me-2 d-inline-flex align-items-center">
-                          <i class="fe fe-download me-2 fs-14"></i> Import
-                        </button>
-                        <button type="button" class="btn btn-white btn-icon-text my-2 me-2 d-inline-flex align-items-center">
-                          <i class="fe fe-filter me-2 fs-14"></i> Filter
-                        </button>
-                        <button type="button" class="btn btn-primary my-2 btn-icon-text d-inline-flex align-items-center">
-                          <i class="fe fe-download-cloud me-2 fs-14"></i> Download Report
-                        </button>
-                    </div>
-                  </div>
                 </div>
 
                 <!-- Page Header Close -->
