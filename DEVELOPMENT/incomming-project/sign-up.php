65e7a5fe8767a31b266fc77dd3be237d903512cf
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-07-22 17:51:18 +0800

    update style and design

--- DEVELOPMENT/incomming-project/sign-up.php
+++ DEVELOPMENT/incomming-project/sign-up.php
@@ -9,46 +9,77 @@
 	<title>Document</title>
 </head>
 <body>
-	<div class="container-fluid mx-auto" style="width: 800px;" id="container">
-		<form action="">
-			<div class="d-flex justify-content-center ">
-				<H2>MRM POWER GENERATION TOOL</H2>								
-			</div>
+<section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black"id="card-body">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-			<hr width="100%">
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-			<div class="row mt-5">
-				<input class="col " type="text" name="userName" placeholder="First Name" >	
-				<input class="col mx-4" type="text" name="userName" placeholder="Last Name" >	
-				<input class="col" type="text" name="userName" placeholder="MI" >	
-			</div>
+                <form class="mx-1 mx-md-4">
 
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                      <input type="text" id="form3Example1c" class="form-control" />
+                      <label class="form-label" for="form3Example1c">Your Name</label>
+                    </div>
+                  </div>
 
-			<div class="row mt-5">
-				<input class="col " type="text" name="userName" placeholder="User Name" >				
-			</div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                      <input type="email" id="form3Example3c" class="form-control" />
+                      <label class="form-label" for="form3Example3c">Your Email</label>
+                    </div>
+                  </div>
 
-			<div class="row mt-5">
-				<input class="col " type="password" name="passWord" placeholder="Password" >				
-			</div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4c" class="form-control" />
+                      <label class="form-label" for="form3Example4c">Password</label>
+                    </div>
+                  </div>
 
-			<div class="row mt-5">
-				<input class="col " type="text" name="emailAddress" placeholder="Email Address" >				
-			</div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4cd" class="form-control" />
+                      <label class="form-label" for="form3Example4cd">Repeat your password</label>
+                    </div>
+                  </div>
 
-			<div class="row mt-5">
-				<input class="col " type="text" name="contactNumber" placeholder="Cellphone Number" >				
-			</div>
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                    <label class="form-check-label" for="form2Example3">
+                      I agree all statements in <a href="#!">Terms of service</a>
+                    </label>
+                  </div>
 
-			<div class="row mt-5">
-				<input class="col " type="text" name="physicalAddress" placeholder="Physical Address" >				
-			</div>
-			<div class="d-flex justify-content-center" id="btn-submit">
-				 <input class="mt-5" type="submit" style="width: 250px;" >
-			</div>
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg">Register</button>
+                  </div>
 
-		</form>
+                </form>
 
-	</div>
+              </div>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="../assets/MRM IMAGES/solar-images-1.jpg"
+                  class="img-fluid" alt="Sample image">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
