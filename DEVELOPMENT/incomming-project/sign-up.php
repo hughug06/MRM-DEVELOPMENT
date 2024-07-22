eac7e60d21f3f998860b69dfdf8f986f7b13c0a8
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-07-22 16:36:49 +0800

    update signup php and css

--- DEVELOPMENT/incomming-project/sign-up.php
+++ DEVELOPMENT/incomming-project/sign-up.php
@@ -5,10 +5,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/signup-style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	
 	<title>Document</title>
 </head>
 <body>
-	<div class="container-fluid mx-auto" style="width: 1500px;" id="container">
+	<div class="container-fluid mx-auto" style="width: 800px;" id="container">
 		<form action="">
 			<div class="d-flex justify-content-center ">
 				<H2>MRM POWER GENERATION TOOL</H2>								
@@ -16,35 +17,36 @@
 
 			<hr width="100%">
 
-			<div class="row mt-3">
-				<div class="col">
-					<input  type="text" name="userName" placeholder="First Name" >	
-				</div>
-				<div class="col">
-					<input  type="text" name="userName" placeholder="Last Name" >	
-				</div>
-				<div class="col">
-					<input  type="text" name="userName" placeholder="MI" >		
-				</div>
-						
+			<div class="row mt-5">
+				<input class="col " type="text" name="userName" placeholder="First Name" >	
+				<input class="col mx-4" type="text" name="userName" placeholder="Last Name" >	
+				<input class="col" type="text" name="userName" placeholder="MI" >	
 			</div>
 
 
-			<div class="row mt-3">
+			<div class="row mt-5">
 				<input class="col " type="text" name="userName" placeholder="User Name" >				
 			</div>
-			<div class="row mt-3">
+
+			<div class="row mt-5">
 				<input class="col " type="password" name="passWord" placeholder="Password" >				
 			</div>
-			<div class="row mt-3">
+
+			<div class="row mt-5">
 				<input class="col " type="text" name="emailAddress" placeholder="Email Address" >				
 			</div>
-			<div class="row mt-3">
+
+			<div class="row mt-5">
 				<input class="col " type="text" name="contactNumber" placeholder="Cellphone Number" >				
 			</div>
-			<div class="row mt-3">
+
+			<div class="row mt-5">
 				<input class="col " type="text" name="physicalAddress" placeholder="Physical Address" >				
 			</div>
+			<div class="row" id="btn-submit">
+				 <input class="mt-5" type="submit" style="width: 250px;" >
+			</div>
+
 		</form>
 
 	</div>
