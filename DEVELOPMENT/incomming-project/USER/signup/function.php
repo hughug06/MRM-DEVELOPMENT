b4ced359aa7cbba0e691f526bcbf140ec66ee40f
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-08-26 03:09:50 +0800

    a

--- DEVELOPMENT/incomming-project/USER/signup/function.php
+++ DEVELOPMENT/incomming-project/USER/signup/function.php
@@ -20,7 +20,7 @@ if(isset($_POST['signup']))
                    VALUES ('$name' , '$username' , '$email' , '$password', '0'  , 'user')";
     $result = mysqli_query($conn , $sql_insert);
     //SUCCESS MESSAGE
-    header("location: /MRM-DEVELOPMENT/DEVELOPMENT/incomming-project/index2.php");
+    header("location: /MRM-DEVELOPMENT/DEVELOPMENT/incomming-project/index.php");
     exit();
   }
 }
