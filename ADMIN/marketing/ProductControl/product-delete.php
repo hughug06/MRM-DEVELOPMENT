86ac46bec30fd1f763bebd09b217335b5c7e4168
Jyonasan59 <joloend.villapando@gmail.com>
2024-08-31 01:24:52 +0800

    add upload image to edit form complete, DELETE, READING OF IMAGE IN EDIT FORM NEEDED

--- ADMIN/marketing/ProductControl/product-delete.php
+++ ADMIN/marketing/ProductControl/product-delete.php
@@ -3,6 +3,7 @@
     include "../../Database/database.php";
     if(isset($_GET['id'])){
         $ProductID = $_GET['id'];
+        $getImage = "Select Image from `products` where ProductID=$ProductID";
         $sql = "DELETE from `products` where ProductID=$ProductID";
         $result = mysqli_query($conn , $sql);
     }
