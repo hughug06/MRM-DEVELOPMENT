08171a0948d23f07be31a5e469bb2fe1f6805e60
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-09-01 05:09:34 +0800

    undone

--- testing.php
+++ testing.php
@@ -1,40 +1,10 @@
-<?php
-//require_once('../Database/database.php');
+<?php 
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '../../vendor/autoload.php';
-
-if(isset($_POST['signup']))
-{
-  
-  $first_name = $_POST['first_name'];
-  $last_name = $_POST['last_name'];
-  $username = $_POST['username'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $verify_token = md5(rand());
-  email_veritication($email);
-    // $sql_select = "select * from users where email='$email' LIMIT 1";
-    // $select_result = mysqli_query($conn , $sql_select);
-    // if(mysqli_num_rows($select_result) > 0)
-    // {
-    //   echo "email_exists"; //return to ajaxx
-    // }
-    // else
-    // {
-    //    email_veritication();
-    //   $sql_insert = "insert into users (firstname,lastname,username,email,password,is_ban,role,verify_token) 
-    //   VALUES ('$first_name' , '$last_name' , '$username' , '$email' , '$password', '0'  , 'client' , '$verify_token')";
-    //   $insert_result = mysqli_query($conn , $sql_insert); 
-      
-    // }
-    
-}
-
-function email_veritication($email){
+require 'vendor/autoload.php';
   $mail = new PHPMailer(true);
 
   try {
@@ -49,7 +19,7 @@ function email_veritication($email){
   
       // Recipients
       $mail->setFrom('janariesimpuerto13@gmail.com', "ARIES"); // Use a valid name
-      $mail->addAddress($email);                                  // Add recipient
+      $mail->addAddress("jonathancarlo.villapando@gmail.com");                                  // Add recipient
   
       // Content
       $mail->isHTML(true);                                        // Set email format to HTML
@@ -70,6 +40,5 @@ function email_veritication($email){
   } catch (Exception $e) {
       echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
   }
-}
 
 ?>
