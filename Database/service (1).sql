c371843a9d2a88697a592be5dd793622c0c2b2de
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-09-06 04:30:58 +0800

    database

--- Database/service (1).sql
+++ Database/service (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 02, 2024 at 08:34 PM
+-- Generation Time: Sep 05, 2024 at 10:30 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,19 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accounts`
+-- Table structure for table `service`
 --
 
-CREATE TABLE `accounts` (
-  `account_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `role` enum('user','admin') DEFAULT 'user',
-  `is_ban` tinyint(1) DEFAULT 0,
-  `account_created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `verify_token` varchar(255) DEFAULT NULL,
-  `verify_status` tinyint(1) DEFAULT 0,
+CREATE TABLE `service` (
+  `service_id` int(11) NOT NULL,
+  `service_name` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -46,33 +39,20 @@ CREATE TABLE `accounts` (
 --
 
 --
--- Indexes for table `accounts`
+-- Indexes for table `service`
 --
-ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`account_id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `user_id` (`user_id`);
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`service_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `accounts`
+-- AUTO_INCREMENT for table `service`
 --
-ALTER TABLE `accounts`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `accounts`
---
-ALTER TABLE `accounts`
-  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`email`) REFERENCES `user_info` (`email`) ON DELETE CASCADE;
+ALTER TABLE `service`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
