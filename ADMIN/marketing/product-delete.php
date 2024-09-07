e36808d4ad1e8126064d77954afb827349644b5e
Jyonasan59 <joloend.villapando@gmail.com>
2024-09-08 04:43:21 +0800

    Error handling in delete wattsKVA complete

--- ADMIN/marketing/product-delete.php
+++ ADMIN/marketing/product-delete.php
@@ -1,11 +1,11 @@
 <?php
-    
+    header('Content-Type: application/json');
     include "../../Database/database.php";
     if(isset($_GET['id'])){
         $ProductID = $_GET['id'];
         $sql = "DELETE from `products` where ProductID=$ProductID";
         $result = mysqli_query($conn , $sql);
+        echo json_encode(['status' => true, 'message' => 'Item deleted successfully']);
     }
-    header('location: marketing-product-control.php');
     exit;
 ?>
