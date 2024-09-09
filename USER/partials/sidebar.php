6f119746237ea5988e25ba6c6fa39cbb5b4f842b
Astelth20 <hollowichigo556@gmail.com>
2024-09-10 01:47:41 +0800

    Added Worker - Admin Category

--- USER/partials/sidebar.php
+++ USER/partials/sidebar.php
@@ -224,6 +224,29 @@
                     </ul>
                 </li>
                 <!-- End::slide -->       
+
+                <!-- Start::slide__category -->
+                <li class="slide__category"><span class="category-name">ADMIN - WORKER</span></li>
+                <!-- End::slide__category -->
+                
+                <li class="slide has-sub <?= $page == '' || $page == '' ? 'active open':''?>">
+                    <a href="javascript:void(0);" class="side-menu__item <?= $page == "" || $page == '' ? 'active':''?>">
+                        <span class="shape1"></span>
+                        <span class="shape2"></span>
+                        <i class="fa-solid fa-screwdriver-wrench side-menu__icon"></i>
+                        <span class="side-menu__label">Services</span>
+                        <i class="fe fe-chevron-right side-menu__angle"></i>
+                    </a>
+                    <ul class="slide-menu child1">
+                        <li class="slide <?= $page == '' ? 'active':''?>">
+                            <a href="" class="side-menu__item <?= $page == '' ? 'active':''?>">Pending</a>
+                        </li> 
+                        <li class="slide <?= $page == "" ? 'active':''?>">
+                            <a href="" class="side-menu__item <?= $page == '' ? 'active':''?>">Completed</a>
+                        </li>                                
+                    </ul>
+                </li>
+
             </ul>
             <div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"> <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path> </svg></div>
         </nav>
