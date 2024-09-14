02f9a64b793cd38d670e05efdb0df4f87d5dae62
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-09-15 02:05:02 +0800

    db

---
+++ Database/service_worker.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2024 at 08:04 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mrm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_worker`
+--
+
+CREATE TABLE `service_worker` (
+  `service_id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `user_id` int(10) NOT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `appointment_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `service_worker`
+--
+ALTER TABLE `service_worker`
+  ADD PRIMARY KEY (`service_id`),
+  ADD UNIQUE KEY `admin_id` (`admin_id`),
+  ADD KEY `account_id` (`account_id`),
+  ADD KEY `fk_appointment` (`appointment_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `service_worker`
+--
+ALTER TABLE `service_worker`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `service_worker`
+--
+ALTER TABLE `service_worker`
+  ADD CONSTRAINT `fk_appointment` FOREIGN KEY (`appointment_id`) REFERENCES `appointments` (`appointment_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `service_worker_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
