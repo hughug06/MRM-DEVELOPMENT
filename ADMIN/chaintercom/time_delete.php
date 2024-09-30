4885a8442ff10ee19be49739b292d2258c7bad55
Jyonasan59 <joloend.villapando@gmail.com>
2024-10-01 05:05:57 +0800

    delete function fixed chaintercom

--- ADMIN/chaintercom/time_delete.php
+++ ADMIN/chaintercom/time_delete.php
@@ -2,9 +2,9 @@
 require_once '../../Database/database.php';
 if(isset($_GET['id'])){
     $id = $_GET['id'];
-    $sql = "DELETE from chaintercom_availability where chaintercomavailid=$id";
+    $sql = "DELETE from chaintercom_availability where chainavailability=$id";
     $result = mysqli_query($conn , $sql);
-    header('location: project-appointment.php');
+    header('location: time-management.php');
     exit;
 }
 
