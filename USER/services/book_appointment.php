092913896a2068d451d47dba0ca967051c6276ba
Jyonasan59 <joloend.villapando@gmail.com>
2024-10-03 00:55:36 +0800

    user service form ajax update

--- USER/services/book_appointment.php
+++ USER/services/book_appointment.php
@@ -33,7 +33,11 @@ if(isset($_POST['book']))
             SET service_count = $service_count + 1
             WHERE account_id  = $account_id;";
             $upd_insert = mysqli_query($conn , $upd);
-
+            if($upd_insert){
+                echo json_encode(['success' => true]);
+            }else{
+                echo json_encode(['message' => 'SQL error inserting']);
+            }
           
 
 
@@ -42,8 +46,10 @@ if(isset($_POST['book']))
             unset($_SESSION['date']);
             unset($_SESSION['start_time']);
             unset($_SESSION['end_time']);
-            header("Location: service.php");
         }       
+    }
+    else{
+        echo json_encode(['message' => 'SQL error retrieving names']);
     }
 ?>
 
