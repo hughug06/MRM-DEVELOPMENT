1021d8caba396a0543758be2fda4db68ec00ecc7
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-10-17 04:33:46 +0800

    update

--- ADMIN/services/set_quotation.php
+++ ADMIN/services/set_quotation.php
@@ -3,29 +3,6 @@
 <?php
 require_once '../../Database/database.php';
 session_start();
-// if(isset($_POST['add']))
-// {
-   
-//     $account_id= $_POST['user_id'];
-//     $appointmentid = $_POST['appointment_id'];    
-//     echo $account_id;
-//     echo $appointmentid;
-//     exit();
-
-//     $insert_pricing = "insert into service_pricing(appointment_id , account_id , amount) 
-//     VALUES('$appointmentid','$account_id','$amount')";
-//     $result = mysqli_query($conn , $insert_pricing);
-//     if($result){
-//         $upd = "UPDATE appointments SET status='Waiting' WHERE appointment_id='$appointmentid'";
-//         $upd_result = mysqli_query($conn , $upd);
-//         header("Location: appointment.php");
-//         exit();
-//     }   
-// }
-
-// Include the database connection file
-
-// Retrieve the data from the form using $_POST
 
 $account_id = $_POST['account_id'];
 $appointment_id = $_POST['appointment_id'];
