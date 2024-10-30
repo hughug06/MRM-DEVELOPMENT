ed431d66980057441aaf2956d800cb7e28850f8b
Astelth20 <hollowichigo556@gmail.com>
2024-10-31 04:09:47 +0800

    admin_project_appoint_update

--- ADMIN/chaintercom/project-appointment.php
+++ ADMIN/chaintercom/project-appointment.php
@@ -61,71 +61,118 @@ require_once '../authetincation.php';
             <!--APP-CONTENT START-->
             <div class="main-content app-content">
                 <div class="container-fluid">
-                    <div class="row row-sm mt-3">
-                        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 grid-margin">
+                    <div class="row square row-sm">
+                        <div class="pt-3 col-lg-12 col-md-12">
                             <div class="card custom-card">
-                                <div class="card-header border-bottom-0 d-block">                            
-                                    <div class="d-flex justify-content-between align-items-center">
-                                    <label class="main-content-label mb-0 pt-2">Project Appointment</label>           
-                                    </div>   
-                                </div> 
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-striped table-bordered table-hover text-center mb-0">
-                                            <thead>
-                                                <tr>
-                                                    <th class="col-lg-2"><span>name</span></th>
-                                                    <th class="col-lg-2"><span>product</span></th>
-                                                    <th class="col-lg-3"><span>meeting link</span></th>
-                                                    <th class="col-lg-2"><span>date</span></th>
-                                                    <th class="col-lg-2"><span>time</span></th>
-                                                    <th class="col-lg-1">status</th>
-                                                    <th class="col-lg-1">action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php 
-                                                require '../../Database/database.php';
-                                                $select = "SELECT * FROM chaintercom_appointment";
-                                                $result = mysqli_query($conn, $select);
-                                                if(mysqli_num_rows($result) > 0) {
-                                                    foreach($result as $resultItem) {
-                                                        ?> 
-                                                        <tr>
-                                                            <td><?= $resultItem['name'] ?></td>    
-                                                            <td><?= $resultItem['product'] ?></td>    
-                                                            <?php
-                                                           
-                                                             if( $resultItem['date'] == date('Y-m-d') ) {?>
-                                                            <td><a href="meeting_room.php?app_id=<?= $resultItem['chaintercomappointid'] ?>&account_id=<?= $resultItem['account_id'] ?>">Meeting link</a></td> 
-                                                            <?php 
-                                                            }
-                                                            else{
+                                <nav class="nav main-nav-line p-3 tabs-menu ">
+                                    <a class="nav-link  active" data-bs-toggle="tab" id="about-tab" href="#pending">Tab 1
+                                        <span class="badge bg-secondary rounded-pill" id="notifiation-data">0</span>
+                                    </a>
+                                    <a class="nav-link" data-bs-toggle="tab" id="about-tab" href="#profile">Tab 2
+                                        <span class="badge bg-secondary rounded-pill" id="notifiation-data">0</span>
+                                    </a>
+                                    <a class="nav-link" data-bs-toggle="tab" id="about-tab" href="#chain">Tab 3
+                                        <span class="badge bg-secondary rounded-pill" id="notifiation-data">0</span>
+                                    </a>
+                                    <a class="nav-link" data-bs-toggle="tab" id="about-tab" href="#completed">Tab 4
+                                        <span class="badge bg-secondary rounded-pill" id="notifiation-data">0</span>
+                                    </a>
+                                    <a class="nav-link" data-bs-toggle="tab" id="about-tab" href="#contact">Tab 5
+                                        <span class="badge bg-secondary rounded-pill" id="notifiation-data">0</span>
+                                    </a>
+                                </nav>
+                            </div>
+                            <div class="row row-sm">
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="card custom-card main-content-body-profile">
+                                        <div class="tab-content">
+                                            <div class="main-content-body tab-pane p-4 border-top-0 active" id="pending">
+                                                <div class="mb-4 main-content-label">Pending</div>
+                                                <div class="card-body border"> 
+                                                    <div class="table-responsive">
+                                                        <table class="table table-striped table-bordered table-hover text-center mb-0">
+                                                            <thead>
+                                                                <tr>
+                                                                    <th class="col-lg-2"><span>name</span></th>
+                                                                    <th class="col-lg-2"><span>product</span></th>
+                                                                    <th class="col-lg-3"><span>meeting link</span></th>
+                                                                    <th class="col-lg-2"><span>date</span></th>
+                                                                    <th class="col-lg-2"><span>time</span></th>
+                                                                    <th class="col-lg-1">status</th>
+                                                                    <th class="col-lg-1">action</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                <?php 
+                                                                require '../../Database/database.php';
+                                                                $select = "SELECT * FROM chaintercom_appointment";
+                                                                $result = mysqli_query($conn, $select);
+                                                                if(mysqli_num_rows($result) > 0) {
+                                                                    foreach($result as $resultItem) {
+                                                                        ?> 
+                                                                        <tr>
+                                                                            <td><?= $resultItem['name'] ?></td>    
+                                                                            <td><?= $resultItem['product'] ?></td>    
+                                                                            <?php
+                                                                        
+                                                                            if( $resultItem['date'] == date('Y-m-d') ) {?>
+                                                                            <td><a href="meeting_room.php?app_id=<?= $resultItem['chaintercomappointid'] ?>&account_id=<?= $resultItem['account_id'] ?>">Meeting link</a></td> 
+                                                                            <?php 
+                                                                            }
+                                                                            else{
 
-                                                            
-                                                            ?>
-                                                             <td>Not yet available</td> 
-                                                             <?php 
-                                                            }
-                                                             ?>
-                                                            <td><?= $resultItem['date'] ?></td>
-                                                            <td><?= $resultItem['start_time'] . " - " . $resultItem['end_time'] ?></td>                        
-                                                            <td><?= $resultItem['status'] ?></td>                          
-                                                            <td>
-                                                                <button class="btn btn-danger d-flex gap-2"><i class="fe fe-trash"></i>DECLINE</button>
-                                                            </td>
-                                                        </tr>   
-                                                        <?php 
-                                                    }
-                                                }
-                                                ?>
-                                            </tbody>    
-                                        </table>
+                                                                            
+                                                                            ?>
+                                                                            <td>Not yet available</td> 
+                                                                            <?php 
+                                                                            }
+                                                                            ?>
+                                                                            <td><?= $resultItem['date'] ?></td>
+                                                                            <td><?= $resultItem['start_time'] . " - " . $resultItem['end_time'] ?></td>                        
+                                                                            <td><?= $resultItem['status'] ?></td>                          
+                                                                            <td>
+                                                                                <button class="btn btn-danger d-flex gap-2"><i class="fe fe-trash"></i>DECLINE</button>
+                                                                            </td>
+                                                                        </tr>   
+                                                                        <?php 
+                                                                    }
+                                                                }
+                                                                ?>
+                                                            </tbody>    
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="main-content-body tab-pane p-4 border-top-0" id="profile">
+                                                <div class="mb-4 main-content-label">Waiting for Payment</div>
+                                                <div class="card-body border">
+                                                    <!-- Content Here -->
+                                                </div>
+                                            </div>
+                                            <div class="main-content-body tab-pane p-4 border-top-0" id="chain">
+                                                <div class="mb-4 main-content-label">Payment Checking</div>
+                                                <div class="card-body border">
+                                                    <!-- Content Here -->
+                                                </div>
+                                            </div>
+                                            <div class="main-content-body tab-pane p-4 border-top-0" id="completed">
+                                                <div class="mb-4 main-content-label">Payment Checking</div>
+                                                <div class="card-body border">
+                                                    <!-- Content Here -->
+                                                </div>
+                                            </div>
+                                            <div class="main-content-body tab-pane p-4 border-top-0" id="contact">
+                                                <div class="mb-4 main-content-label">Cancelled Appointment</div>
+                                                <div class="card-body border">
+                                                    <!-- Content Here -->
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>                         
+                    </div>
                 </div>
             </div>
             <!--APP-CONTENT CLOSE-->
