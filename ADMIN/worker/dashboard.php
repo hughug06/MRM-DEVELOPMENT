29b00525ca3100f048ccbbf4c49f157c137c032e
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-04 00:37:52 +0800

    d

--- ADMIN/worker/dashboard.php
+++ ADMIN/worker/dashboard.php
@@ -100,7 +100,7 @@ require_once '../../Database/database.php';
                                         </p>
                                         <?php 
                                         $currentDate = date('Y-m-d');
-                                            if($serviceitem['date'] != $currentDate){                                                                                        
+                                            if($serviceitem['date'] == $currentDate){                                                                                        
                                         ?>
                                          
                                          <a href="service_complete.php?account_id=<?= $serviceitem['account_id']?>&&appointment_id=<?= $serviceitem['appointment_id']?>&&payment_id=<?= $serviceitem['payment_id']?>" class="btn btn-primary">Done</a>  
