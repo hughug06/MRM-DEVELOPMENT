d5f1f580e54e83ada99d3c06f9b5aa3be59300e1
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-05 01:22:58 +0800

    database

--- Database/database.php
+++ Database/database.php
@@ -7,7 +7,7 @@
 
 // Check the connection$mysqli = new mysqli("localhost","my_user","my_password","my_db");
 
-    $conn = new mysqli('localhost', 'u103590962_mrm', 'ZO&aSqn^uS/b', 'u103590962_mrm');
+    $conn = new mysqli('localhost', 'mrm', 'ZO&aSqn^uS/b', 'mrm');
     IF($conn->connect_error){
         die("Something went wrong");
         }
