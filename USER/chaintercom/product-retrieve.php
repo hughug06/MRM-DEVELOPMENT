44e7bef977a7e2afd5448cd9d4f1c669e347207b
Jyonasan59 <joloend.villapando@gmail.com>
2024-11-05 04:42:08 +0800

    product test

--- USER/chaintercom/product-retrieve.php
+++ USER/chaintercom/product-retrieve.php
@@ -4,7 +4,6 @@ require_once '../../Database/database.php';
 $sql = "SELECT * FROM PRODUCTS WHERE Availability = 1";
 $result = mysqli_query($conn, $sql);
 
-echo("EYYY");
 if ($result) {
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
