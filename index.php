f78981374cb004663bf52d33ad06272763d0699e
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-05 02:58:10 +0800

    test

--- index.php
+++ index.php
@@ -1163,7 +1163,7 @@
 
           $.ajax({
             type: "POST",
-            url: "/mrm-eg.online/USER/signup/function.php",
+            url: "USER/signup/function.php",
             data: formData,
             dataType: "json",
             beforeSend: function () {
