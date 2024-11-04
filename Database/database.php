82c2f7d9f886352dc8bb788538a54331e3afffc1
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-05 01:20:10 +0800

    test

--- Database/database.php
+++ Database/database.php
@@ -1,14 +1,13 @@
 <?php 
-// Replace these with your Hostinger database credentials
 
-// Establishing a connection $mysqli = new mysqli("localhost","my_user","my_password","my_db");
+
+
+
 
 
 // Check the connection$mysqli = new mysqli("localhost","my_user","my_password","my_db");
 
-
-
-    $conn = new mysqli('mrm-eg.online', 'mrm', 'ZO&aSqn^uS/b', 'u103590962_mrm');
+    $conn = new mysqli('mrm-eg.online', 'u103590962_mrm', 'ZO&aSqn^uS/b', 'u103590962_mrm');
     IF($conn->connect_error){
         die("Something went wrong");
         }
