43423dda6ad73dd23b6862f8c9678616e5353182
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-05 04:52:23 +0800

    test

--- USER/chaintercom/chaintercom.php
+++ USER/chaintercom/chaintercom.php
@@ -177,7 +177,25 @@ if (isset($_GET['chainavailability'], $_GET['date'], $_GET['start_time'], $_GET[
 
                     
                     <div class="input-field">
-                        <?php include '/chaintercom/product-retrieve.php'; ?>
+                    <?php 
+                
+                     
+                        $sql = "SELECT * FROM products WHERE Availability = 1";
+                        $result = mysqli_query($conn, $sql);
+
+                        if ($result) {
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo '<div class="form-check product-item">';
+                                    echo '<input class="form-check-input" type="checkbox" id="product_' . $row['ProductID'] . '" name="selected_products[]" value="' . $row['ProductID'] . '">';
+                                    echo '<label class="form-check-label product-name" for="product_' . $row['ProductID'] . '">' . $row['ProductName'] . '</label>';
+                                    echo '<span class="specification"><br> SPECIFICATION: ' . htmlspecialchars($row['Specification']) . '</span>'; // Display specification
+                                    echo '</div>';
+                                }
+                            }     
+                        }
+
+                        ?>
                     </div>
                 </div>
                   
