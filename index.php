567551925465301f5c33b76b1bb2a408e84c0e1f
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-05 01:15:08 +0800

    test

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a href="#" aria-current="page" class="nav-link">Home</a>
+              <a href="#" aria-current="page" class="nav-link">HOMEEEEEE</a>
             </li>
             <li class="nav-item">
               <a href="#Details" class="nav-link">Details</a>
