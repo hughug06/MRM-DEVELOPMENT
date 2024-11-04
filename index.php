5085fbb261ded6ee68f58240692eb65564af6565
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-05 01:21:06 +0800

    test

--- index.php
+++ index.php
@@ -33,13 +33,7 @@
     <link rel="stylesheet" href="assets/landing_assets/css/styles.css" />
     <link rel="icon" href="assets/landing_assets/images/logo-mrm.png" />
     <title>
-    <?php 
-    require_once 'database/database.php';
-    $sql = "select * from accounts";
-    $result = mysqli_query($conn , $sql);
-    $row = mysqli_fetch_assoc($result);
-    ?>
-    <?= $row['email']?></title>
+    MRM-EG Company</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg sticky-top navbar-light">
