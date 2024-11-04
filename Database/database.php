b1fa0f488239944cc21b8d4a669d050fdae83828
hughug06 <59154192+hughug06@users.noreply.github.com>
2024-11-05 03:01:05 +0800

    test

--- Database/database.php
+++ Database/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'u103590962_mrm', 'Mrmeg123', 'u103590962_mrmeg');
+$conn = new mysqli('localhost', 'asdasd', 'Mrmeg123', 'u103590962_mrmeg');
 
 // Check the connection
 if ($conn->connect_error) {
