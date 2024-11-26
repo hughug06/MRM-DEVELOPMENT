9fc3f08b25ad410540af44cf0bd8f2aa1c81b171
Jyonasan59 <joloend.villapando@gmail.com>
2024-11-26 22:59:56 +0800

    ajax on reject project by admin

--- ADMIN/logout/function.php
+++ ADMIN/logout/function.php
@@ -4,6 +4,10 @@
         unset($_SESSION['role']);
         unset($_SESSION['user_id']);
         unset($_SESSION['auth']);
+        unset($_SESSION['admin_id']);
+        unset($_SESSION['account_id']);
+        unset($_SESSION['agent_id']);
+        unset($_SESSION['worker_id']);
         header('Content-Type: application/json');
         echo json_encode(['success' => true]);
         exit();
