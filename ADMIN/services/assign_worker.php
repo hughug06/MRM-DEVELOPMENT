c9f7dad39f9fb661177f3536c78cfd46df5175d7
Jyonasan59 <joloend.villapando@gmail.com>
2024-12-03 02:33:32 +0800

    asd

--- ADMIN/services/assign_worker.php
+++ ADMIN/services/assign_worker.php
@@ -11,19 +11,27 @@ if (isset($_POST['pick'])) {
     // Echo the variables in a readable format
     $sql = "INSERT INTO `worker_ongoing`(`booking_id`, `admin_id`, `client_id`, `availability_id`, `worker_id`)
             VALUES ('$booking_id','$admin_id','$client_id','$availability_id','$user_id')";
+    if(mysqli_query($conn , $sql)){
+        $working_work_id =  mysqli_insert_id($conn);
+    }
     $sql2 = "UPDATE worker_availability SET is_available='0' WHERE user_id= $user_id";
     $sql3 = "UPDATE service_booking SET booking_status='ongoing' , payment_status = 'delivery_payment' WHERE booking_id= $booking_id";
     $sql4 = "UPDATE kanban SET kanban_status='pick_up' WHERE booking_id= $booking_id";
      $notification = "INSERT INTO notification (message, `from`, user_id)
              VALUES ('Booking approved', 'payment approved', '$client_id')";
 
-    $result = mysqli_query($conn , $sql);
+    
+    $sql5 = "INSERT INTO `service_history`(`booking_id`, `working_id`, `pick_up`)
+            VALUES ('$booking_id','$working_work_id', NOW())";
+             
+
     $result2 = mysqli_query($conn , $sql2);
     $result3 = mysqli_query($conn , $sql3);
     $result4 = mysqli_query($conn , $sql4);
     $result5 = mysqli_query($conn , $notification);
+    $result6 = mysqli_query($conn , $sql5);
 
-    if($result && $result2 && $result3 && $result4 && $result5){
+    if($result2 && $result3 && $result4 && $result5){
         echo json_encode(['success' => true,]);
     }else{
         echo json_encode(['success' => false, 'message' => 'Error in SQL']);
