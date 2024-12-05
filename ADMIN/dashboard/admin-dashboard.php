9b3b7a7dc3e8d5bbbbb20e5ff68911700e6ad16f
Jyonasan59 <joloend.villapando@gmail.com>
2024-12-05 13:42:51 +0800

    asd

--- ADMIN/dashboard/admin-dashboard.php
+++ ADMIN/dashboard/admin-dashboard.php
@@ -100,6 +100,18 @@ GROUP BY
         <!-- Choices Css -->
         <link rel="stylesheet" href="../../assets/libs/choices.js/public/assets/styles/choices.min.css">
 
+        <script type="text/javascript">
+                                    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+                                    (function() {
+                                        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                                        s1.async = true;
+                                        s1.src = 'https://embed.tawk.to/6730d9822480f5b4f59b5f52/default';
+                                        s1.charset = 'UTF-8';
+                                        s1.setAttribute('crossorigin', '*');
+                                        s0.parentNode.insertBefore(s1, s0);
+                                    })();
+                                </script>
+
     </head>
 
     <body>
