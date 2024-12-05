a4f94bdf9fc869cd46083a0ba01547e9796b4d35
Jyonasan59 <joloend.villapando@gmail.com>
2024-12-05 13:57:06 +0800

    asd

--- ADMIN/dashboard/admin-dashboard.php
+++ ADMIN/dashboard/admin-dashboard.php
@@ -114,17 +114,16 @@ GROUP BY
             <!-- End::app-sidebar -->
 
             <script type="text/javascript">
-                                    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-                                    (function() {
-                                        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-                                        s1.async = true;
-                                        s1.src = 'https://embed.tawk.to/6730d9822480f5b4f59b5f52/default';
-                                        s1.charset = 'UTF-8';
-                                        s1.setAttribute('crossorigin', '*');
-                                        s0.parentNode.insertBefore(s1, s0);
-                                    })();
-                                    Tawk_API.showWidget();
-                                </script>
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6744c1904304e3196ae86e53/1idi987he';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
 
             <div class="main-content app-content">
                 <div class="container-fluid">
