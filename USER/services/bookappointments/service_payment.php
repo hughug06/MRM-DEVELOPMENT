4bd5cf751b36957e78976cbef4e634f678ca3669
Astelth20 <hollowichigo556@gmail.com>
2024-12-05 17:06:47 +0800

    asd

--- USER/services/bookappointments/service_payment.php
+++ USER/services/bookappointments/service_payment.php
@@ -1128,9 +1128,9 @@ else if(isset($_POST['tuneup_submit'])){
         const element = document.getElementById('cardToDownload');
         const opt = {
             margin: 1,
-            filename: 'card-details.pdf',
+            filename: 'Quotation.pdf',
             image: { type: 'jpeg', quality: 0.98 },
-            html2canvas: { scale: 2 },
+            html2canvas: { scale: 1 },
             jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
         };
         html2pdf().set(opt).from(element).save();
