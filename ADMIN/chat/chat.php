caa3d0aeea816e96d2cd5ac4a1e626c2fb20b2bd
Jyonasan59 <joloend.villapando@gmail.com>
2024-12-05 13:36:59 +0800

    test

--- ADMIN/chat/chat.php
+++ ADMIN/chat/chat.php
@@ -75,6 +75,17 @@ include_once '../../Database/database.php';
                             <!-- Embed Tawk.to Admin Dashboard -->
                             <iframe src="https://dashboard.tawk.to/"></iframe>
                            
+                                <script type="text/javascript">
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/YOUR_PROPERTY_ID/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
                         </div>
                     </div><!-- COL END -->
                 </div>
@@ -147,7 +158,7 @@ include_once '../../Database/database.php';
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <script>
-
+        
     </script>
 
 
